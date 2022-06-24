dd8a95b03cc9a862c1a22c759775da2b6bad8178
Anil <anilbudhathokibg@gmail.com>
2022-06-24 09:41:03 +0545

    change to models

--- server/class/models.py
+++ server/class/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from user.models import Student
 from user.models import Teacher
 class Class(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=150)
     student_id = models.ForeignKey(Student, on_delete=models.PROTECT)
     teacher_id = models.ForeignKey(Teacher, on_delete=models.PROTECT)
 
